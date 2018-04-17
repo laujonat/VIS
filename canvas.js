081c79b6746f1dbb8a4fe40f56d52d4a2a653a70
Jonathan Lau <jonhlau28@gmail.com>
2018-04-16 21:28:13 -0700

    initial commit

---
+++ canvas.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+console.log(canvas);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const c = canvas.getContext('2d');
