4f974f99f120675728f4ec89721b6e846834dd2b
Jonathan Lau <jonhlau28@gmail.com>
2018-04-16 22:42:54 -0700

    add basic canvas animations

--- canvas.js
+++ canvas.js
@@ -1,7 +1,70 @@
 const canvas = document.querySelector('canvas');
-console.log(canvas);
+
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 const c = canvas.getContext('2d');
+
+
+
+
+
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = () => {
+    c.beginPath();
+    // arc (x, y, radius, start angle, end angle, counterclockwise)
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'pink';
+    c.stroke();
+    c.fillStyle = 'purple';
+    c.fill();
+  };
+
+  this.update = () => {
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
+}
+
+
+let circleArray = [];
+for (let i = 0; i < 100; ++i) {
+  let radius = 10;
+  // subtract diameter and add radius to make sure circle does not spawn on edge of screen
+  let x = Math.random() * (canvas.width - radius * 2) + radius;
+  let y = Math.random() * (canvas.height - radius * 2) + radius;
+  let dx = (Math.random() - 0.5);
+  let dy = (Math.random() - 0.5);
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+console.log(circleArray);
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  for( let i = 0; i < circleArray.length; ++i) {
+    circleArray[i].update();
+  }
+
+}
+
+animate();
